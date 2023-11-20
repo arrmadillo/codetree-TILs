ed69dbb458d37d4c94bd2de130546bb9104cfd7f
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2023-11-20 22:35:41 +0900

    Title: 시험 통과 여부 확인하기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231120/시험 통과 여부 확인하기/verify-test-passed.py
@@ -0,0 +1,12 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    n = int(input())
+    if n > 80:
+        print('pass')
+        return
+    print(f'{80-n} more score')
+
+solution()
