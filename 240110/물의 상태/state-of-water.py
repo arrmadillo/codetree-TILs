31dce1eca5cbca8b69d89819ad585413fa22aa1c
arrmadillo <105644827+arrmadillo@users.noreply.github.com>
2024-01-10 23:16:11 +0900

    Title: 물의 상태, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240110/물의 상태/state-of-water.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+def solution():
+    temp = int(input())
+    if temp < 0:
+        return 'ice'
+    elif temp > 100:
+        return 'vapor'
+    return 'water'
+
+print(solution())
